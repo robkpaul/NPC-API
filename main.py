04e876185c33eff71d6079d58cb8358e07b998f2
robkpaul <rob.k.paul@gmail.com>
2021-10-04 23:03:30 -0700

    name and race implementation

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 from fastapi import FastAPI
+import json, random
 
 app = FastAPI()
 
-db = []
+class NPC:
+    name = ""
+    race = ""
+
+    
+
+
+names = open("./resources/names.json")
+names = json.load(names)
 
 @app.get('/')
 def index():
     return {'key' : 'value'}
 
-@app.get('/npc')
-def index():
-    return {'name' : 'bob'}
+@app.get('/new')
+def newNPC():
+    npc = NPC()
+    randRace = random.randint(1,10)
+    if randRace == 1:
+        npc.race = "Dwarf"
+    elif randRace == 2:
+        npc.race = "Elf"
+    elif randRace == 3:
+        npc.race = "Halfling"
+    elif randRace == 4:
+        npc.race = "Dragonborn"
+    elif randRace == 5:
+        npc.race = "Gnome"
+    elif randRace == 6:
+        npc.race = "Tiefling"
+    elif randRace > 6:
+        npc.race = "Human"
+    
+    npc.name = names[npc.race+"_first"][random.randint(0,len(names[npc.race+"_first"])-1)]
+    if(npc.race != "Elf" and npc.race != "Tiefling"):
+        npc.name = npc.name + " " + names[npc.race+"_last"][random.randint(0,len(names[npc.race+"_last"])-1)]
+
+    return npc
