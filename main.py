1dc19aa0321fddc4f555c489d45b6391b0553bac
robkpaul <rob.k.paul@gmail.com>
2021-10-05 08:57:49 -0700

    Early implementation

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 import json, random
 
 app = FastAPI()
 
 class NPC:
-    name: str
-    race: str
+    name = ''
+    race = ''
     stats = {'str': 10, 'dex': 10, 'con': 10, 'int': 10, 'wis': 10, 'cha': 10}
 
-    def asdict():
-        return {'name': self.name, 'race': self.race, 'stats': self.stats}
-
+    def toDict(self):
+        as_dict = {}
+        as_dict['name'] = self.name
+        as_dict['race'] = self.race
+        as_dict['stats'] = {'str': self.stats['str'], 
+                            'dex': self.stats['dex'], 
+                            'con': self.stats['con'], 
+                            'int': self.stats['int'], 
+                            'wis': self.stats['wis'], 
+                            'cha': self.stats['cha'],
+                            }
+        return as_dict
 
 names = open('./resources/names.json')
 names = json.load(names)
@@ -22,27 +33,27 @@ def index():
 @app.get('/new')
 def newNPC():
     npc = NPC()
-    randRace = random.randint(1,10)
-    if randRace == 1:
+    rand_race = random.randint(1,10)
+    if rand_race == 1:
         npc.race = 'Dwarf'
-    elif randRace == 2:
+    elif rand_race == 2:
         npc.race = 'Elf'
-    elif randRace == 3:
+    elif rand_race == 3:
         npc.race = 'Halfling'
-    elif randRace == 4:
+    elif rand_race == 4:
         npc.race = 'Dragonborn'
-    elif randRace == 5:
+    elif rand_race == 5:
         npc.race = 'Gnome'
-    elif randRace == 6:
+    elif rand_race == 6:
         npc.race = 'Tiefling'
-    elif randRace > 6:
+    elif rand_race > 6: # ~40% chance of human
         npc.race = 'Human'
 
     npc.name = names[npc.race+'_first'][random.randint(0,len(names[npc.race+'_first'])-1)]
-    if(npc.race != 'Elf' and npc.race != 'Tiefling'):
+    if(npc.race != 'Elf' and npc.race != 'Tiefling'): # chooses last name of NPC, elves and tieflings have no last names, so ignores them.
         npc.name = npc.name + ' ' + names[npc.race+'_last'][random.randint(0,len(names[npc.race+'_last'])-1)]
 
-    for stat in npc.stats:
+    for stat in npc.stats: # randomizing stats: maximum 12, minimum 8 (assuming commoner stats for npcs)
         npc.stats[stat] = npc.stats[stat]-random.randint(-2,2)
 
-    return npc.asdict()
+    return npc.toDict()
