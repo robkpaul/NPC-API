c5c9877978519823f27a1b1e550b0dac3b474024
robkpaul <rob.k.paul@gmail.com>
2021-10-05 00:05:20 -0700

    Quite honestly, a bit confused.

--- main.py
+++ main.py
@@ -4,40 +4,45 @@ import json, random
 app = FastAPI()
 
 class NPC:
-    name = ""
-    race = ""
+    name: str
+    race: str
+    stats = {'str': 10, 'dex': 10, 'con': 10, 'int': 10, 'wis': 10, 'cha': 10}
 
-    
+    def asdict():
+        return {'name': self.name, 'race': self.race, 'stats': self.stats}
 
 
-names = open("./resources/names.json")
+names = open('./resources/names.json')
 names = json.load(names)
 
 @app.get('/')
 def index():
-    return {'key' : 'value'}
+    return 'active'
 
 @app.get('/new')
 def newNPC():
     npc = NPC()
     randRace = random.randint(1,10)
     if randRace == 1:
-        npc.race = "Dwarf"
+        npc.race = 'Dwarf'
     elif randRace == 2:
-        npc.race = "Elf"
+        npc.race = 'Elf'
     elif randRace == 3:
-        npc.race = "Halfling"
+        npc.race = 'Halfling'
     elif randRace == 4:
-        npc.race = "Dragonborn"
+        npc.race = 'Dragonborn'
     elif randRace == 5:
-        npc.race = "Gnome"
+        npc.race = 'Gnome'
     elif randRace == 6:
-        npc.race = "Tiefling"
+        npc.race = 'Tiefling'
     elif randRace > 6:
-        npc.race = "Human"
-    
-    npc.name = names[npc.race+"_first"][random.randint(0,len(names[npc.race+"_first"])-1)]
-    if(npc.race != "Elf" and npc.race != "Tiefling"):
-        npc.name = npc.name + " " + names[npc.race+"_last"][random.randint(0,len(names[npc.race+"_last"])-1)]
+        npc.race = 'Human'
 
-    return npc
+    npc.name = names[npc.race+'_first'][random.randint(0,len(names[npc.race+'_first'])-1)]
+    if(npc.race != 'Elf' and npc.race != 'Tiefling'):
+        npc.name = npc.name + ' ' + names[npc.race+'_last'][random.randint(0,len(names[npc.race+'_last'])-1)]
+
+    for stat in npc.stats:
+        npc.stats[stat] = npc.stats[stat]-random.randint(-2,2)
+
+    return npc.asdict()
